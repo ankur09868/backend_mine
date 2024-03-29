96fcaaa0b8c40286d284a1a87e37f8bad3d8a521
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-03-29 16:00:41 +0530

    meetings added

---
+++ meetings/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from contacts.models import Contact
+from django.conf import settings
+from django.db import models
+
+class meetings(models.Model):
+    title = models.CharField(max_length=64, blank=True, null=True, verbose_name='Title')
+    location = models.CharField(max_length=255, blank=True, null=True, verbose_name='Location')
+    from_time = models.DateTimeField(verbose_name='From')
+    to_time = models.DateTimeField(verbose_name='To')
+    related_to = models.CharField(max_length=255, blank=True, null=True, verbose_name='Related To')
+    contact_name = models.ForeignKey(Contact, on_delete=models.SET_NULL, related_name='meeting_contacts', blank=True, null=True, verbose_name='Contact Name')
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='meeting_hosts', blank=True, null=True, verbose_name='Host')
+    participants = models.ManyToManyField(Contact, related_name='meeting_participants', blank=True)
+    description = models.TextField(blank=True, null=True, verbose_name='Description')
+    account = models.ForeignKey('accounts.Account', related_name='meetings', on_delete=models.CASCADE, blank=True, null=True)
+    assigned_to = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='meeting_assigned_users',blank=True, null=True)
+    createdBy = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='meeting_created_by', on_delete=models.CASCADE,blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- simplecrm/urls.py
+++ simplecrm/urls.py
@@ -20,11 +20,13 @@ from accounts import views as aviews
 from leads import views as lviews
 from opportunities import views as oviews
 from contacts import views as cviews
+from meetings import views as mviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'accounts/', aviews.AccountListAPIView.as_view(), name='account-list'),
     path(r'leads/', lviews.LeadListAPIView.as_view(), name='lead-list'),
     path(r'opportunities/', oviews.OpportunityListAPIView.as_view(), name='opportunity-list'),
-    path(r'contacts/', cviews.ContactListAPIView.as_view(), name='contact-list')
+    path(r'contacts/', cviews.ContactListAPIView.as_view(), name='contact-list'),
+    path('meetings/', mviews.MeetingListAPIView.as_view(), name='meeting-list'),
 ]
