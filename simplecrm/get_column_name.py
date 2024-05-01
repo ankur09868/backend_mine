1aea9082c3635db768e66f025ac3aa6e24391417
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-05-01 15:40:20 +0530

    ehehehe

--- simplecrm/get_column_name.py
+++ simplecrm/get_column_name.py
@@ -8,17 +8,17 @@ from django.views.decorators.csrf import csrf_exempt
 def get_excel_columns(request):
     if request.method == 'POST' and request.FILES.get('file'):
         excel_file = request.FILES['file']
-        
+        num = int(request.POST.get('startrow'))
         try:
             file_extension = os.path.splitext(excel_file.name)[1].lower()
-            encoding = 'utf-8' if file_extension in ('.xlsx', '.xls', '.csv') else None
+            encoding = 'latin1' if file_extension in ('.xlsx', '.xls', '.csv') else None
 
             if file_extension == '.xlsx':
-                df = pd.read_excel(excel_file, engine='openpyxl', encoding=encoding, header=3)
+                df = pd.read_excel(excel_file, engine='openpyxl', header=num)
             elif file_extension == '.xls':
-                df = pd.read_excel(excel_file, encoding=encoding, header=3)
+                df = pd.read_excel(excel_file, header=num)
             elif file_extension == '.csv':
-                df = pd.read_csv(excel_file, encoding=encoding, header=3)
+                df = pd.read_csv(excel_file, header=num)
             else:
                 return JsonResponse({'error': 'Unsupported file format'}, status=400)
 
