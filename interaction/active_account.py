dffcad622697167e9428cba4dbc1583a34911ab6
Ankur <ankury639@gmail.com>
2024-06-20 20:30:52 +0530

    lead_sum

--- interaction/active_account.py
+++ interaction/active_account.py
@@ -9,6 +9,8 @@ from django.http import JsonResponse
 from interaction.models import Interaction
 from accounts import serializers as accser
 from contacts import serializers as conser
+from stage.models import Stage
+
 AccountSerializer=accser.AccountSerializer
 ContactSerializer=conser.ContactSerializer
 
@@ -86,36 +88,45 @@ def get_most_active_contacts(request):
     except Exception as e:
         print("Error:", e)
         return JsonResponse({'message': 'An error occurred.'}, status=500)
+    
 
 def get_lead_summation(request):
     try:
-        stages = Lead.objects.values('status').annotate(
-            total_amount=Sum('opportunity_amount'),
-            lead_count=Count('id')
+        # Filter stages to include only those related to leads
+        lead_stages = Stage.objects.filter(model_name='lead')
+
+        # Aggregate the total opportunity amount and lead count for each stage
+        stages = lead_stages.values('status').annotate(
+            total_amount=Sum('lead__opportunity_amount'),
+            lead_count=Count('lead__id')
         ).order_by('status')
         
+        # Initialize the status dictionary with all possible lead statuses
         status_dict = {status[0]: {'total_amount': 0, 'lead_count': 0} for status in LEAD_STATUS}
 
+        # Update the status dictionary with aggregated data from the query
         for stage in stages:
             status_dict[stage['status']] = {
-                'total_amount': stage['total_amount'],
-                'lead_count': stage['lead_count']
+                'total_amount': stage['total_amount'] or 0,
+                'lead_count': stage['lead_count'] or 0
             }
 
+        # Prepare the response data with all possible statuses
         stages_with_all_statuses = [
-            {'status': status, 
+            {'stage': status, 
              'total_amount': data['total_amount'], 
              'lead_count': data['lead_count']
             }
             for status, data in status_dict.items()
         ]
         
-        total_amount = Lead.objects.aggregate(total_amount=Sum('opportunity_amount'))['total_amount']
+        # Aggregate the total opportunity amount for all leads
+        total_amount = lead_stages.aggregate(total_amount=Sum('lead__opportunity_amount'))['total_amount']
 
         return JsonResponse({
             'lead_sum': {
                 'stages': stages_with_all_statuses,
-                'total_amount': total_amount,
+                'total_amount': total_amount or 0,
             }
         }, safe=False)
     except Exception as e:
