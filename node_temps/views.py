66312be7e3b7f6ac0cee891ec96e65491a8620f6
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-06-27 16:23:07 +0530

    message

--- node_temps/views.py
+++ node_temps/views.py
@@ -6,12 +6,11 @@ from rest_framework.permissions import IsAuthenticated
 class NodeTemplateListCreateAPIView(generics.ListCreateAPIView):
     queryset = NodeTemplate.objects.all()
     serializer_class = NodeTemplateSerializer
-    permission_classes = [IsAuthenticated]
+    
 
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
+   
 
 class NodeTemplateDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = NodeTemplate.objects.all()
     serializer_class = NodeTemplateSerializer
-    permission_classes = [IsAuthenticated]
+  
