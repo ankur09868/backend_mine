3571047d5a22318c5a88f0a7829aae826f21d820
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-09-05 10:53:21 +0530

    05.9.24

--- startup.sh
+++ startup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-source /home/site/wwwroot/antenv/bin/activate
-exec gunicorn --bind 0.0.0.0:8000 simplecrm.wsgi:application
+
+# Navigate to the application directory
+cd /home/site/wwwroot
+
+# Create and activate the virtual environment
+python -m venv antenv
+source antenv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Start the Django application
+python manage.py runserver 0.0.0.0:8000
